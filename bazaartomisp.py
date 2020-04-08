33a777e08ac006f471280bb7921d1d54c90fa714
DocArmoryTech <docarmorytech@gmail.com>
2020-04-08 15:29:52 +0100

    Tidy up and overhaul to simplify
    Removed the two stage building of file objects.  
    
    The initial bazaar search returns insufficient data to adequately detail file objects or the relationships between them. Available data had been used to create a 'base' file object that was then enriched with a second API call. Now, the initial search is pretty much used for sha256s only; file objects are then built in one shot from a hash-specific API call.
    
    Relationships between samples/files are followed and details recursively; recursion stops when a loop is (primitively) detected.

--- bazaartomisp.py
+++ bazaartomisp.py
@@ -23,20 +23,95 @@ bazaar_query={'query':'get_taginfo', 'tag':'COVID-19'}
 API_REF_CONTEXTS=['dropped_by_sha256','dropping_sha256']
 API_LINK_CONTEXTS=['urlhaus','any_run','joe_sandbox','malpedia','twitter','links']
 
-def genFileObj(sample):
-    sampl = GenericObjectGenerator('file')
-    sampl.add_attribute("md5", value=sample['md5_hash'], to_ids=True)
-    sampl.add_attribute("filename", value=sample['file_name'], to_ids=False, disable_correlation=True)
-    sampl.add_attribute("sha1", value=sample['sha1_hash'], to_ids=True)
-    sampl.add_attribute("sha256", value=sample['sha256_hash'], to_ids=True)
-    sampl.add_attribute("ssdeep", value=sample['ssdeep'], to_ids=True)
-    sampl.add_attribute("size-in-bytes", value=sample['file_size'], to_ids=False, disable_correlation=True)
-    sampl.add_attribute("state", value="Malicious", to_ids=False, disable_correlation=True)
-    sampl.add_attribute("mimetype", value=sample['file_type_mime'].replace('\\',''), to_ids=False, disable_correlation=True)
+def addSampleByHash(hashes, event):
+    hash=""
+    if ( type(hashes) is list):
+        hash=hashes[0]
+    elif ( type(hashes) is str):
+        hash=hashes
+        hashes=[hash]
 
+    sample_json = _getSampleJson(hash)
+
+    if (sample_json is None):
+        return
+
+    sampl = GenericObjectGenerator('file')
+
+    sampl.add_attribute("md5", value=sample_json['md5_hash'], to_ids=True)
+    sampl.add_attribute("filename", value=sample_json['file_name'], to_ids=False, disable_correlation=True)
+    sampl.add_attribute("sha1", value=sample_json['sha1_hash'], to_ids=True)
+    sampl.add_attribute("sha256", value=sample_json['sha256_hash'], to_ids=True)
+    sampl.add_attribute("ssdeep", value=sample_json['ssdeep'], to_ids=True)
+    sampl.add_attribute("size-in-bytes", value=sample_json['file_size'], to_ids=False, disable_correlation=True)
+    sampl.add_attribute("state", value="Malicious", to_ids=False, disable_correlation=True)
+    sampl.add_attribute("mimetype", value=sample_json['file_type_mime'].replace('\\',''), to_ids=False, disable_correlation=True)
+
+    # if it exists, add the comment
+    if ( 'comment' in sample_json ):
+        comment=sample_json['comment']
+        if ( comment is not None ) and (len(comment) > 0):
+            commattrs=pm.freetext(event, comment)
+
+            for commattr in commattrs:
+               if (commattr['value'] in attributes):
+                   attr=attributes[commattr['value']]
+                   sampl.add_reference(referenced_uuid=attr.uuid, relationship_type='related-to')
+               else:
+                   attr=event.add_attribute(commattr['type'], commattr['value'])
+                   attributes.update({commattr['value']:attr})
+                   sampl.add_reference(referenced_uuid=attr.uuid, relationship_type='related-to')
+
+    # find and add x-references
+    if ( 'file_information' in sample_json):
+            info=sample_json['file_information']
+            if (info is not None):
+                for context_set in info:
+                    context=context_set['context']
+                    value=context_set['value']
+                    print("context: {}, value: {}".format(context, value))
+                    if ( context in API_REF_CONTEXTS ):
+                        ref_uuid=""
+
+                        addedSample=None
+                        # if referenced sample is not already represented, recursively create it and add to the event and to samples<dict>
+                        if (( value not in samples ) and (value not in hashes) ) :
+                            addedSample=addSampleByHash([value]+hashes, event)
+
+                        if (addedSample is not None):
+                            ref_uuid=samples[value].uuid
+
+                            if (context == "dropped_by_sha256"):
+                                sampl.add_reference(referenced_uuid=ref_uuid, relationship_type='dropped-by')
+                            else:
+                                sampl.add_reference(referenced_uuid=ref_uuid, relationship_type='drops')
+                    elif ( context.casefold() in API_LINK_CONTEXTS ):
+                        url_ref=value.replace('\\','')
+                        attribute = None
+                        if ( url_ref not in attributes):
+                            attribute = event.add_attribute('url', url_ref, to_ids=False, disable_correlation=True)
+                            attributes.update({attribute.value : attribute})
+                            sampl.add_reference(referenced_uuid=attribute.uuid, relationship_type='related-to')
+                        else:
+                            sampl.add_reference(referenced_uuid=attributes[url_ref].uuid, relationship_type='related-to')
+                    else:
+                        print("Lost context: {}".format(context))
+
+    attribute = None
+    report_url="https://bazaar.abuse.ch/sample/{}/".format(hash)
+    if (report_url not in attributes):
+        attribute = event.add_attribute("url", "https://bazaar.abuse.ch/sample/{}/".format(hash), to_ids = False, disable_correlation=True)
+        attributes.update({attribute.value : attribute})
+    else:
+        attribute=attributes[report_url]
+
+    sampl.add_reference(referenced_uuid=attribute.uuid, relationship_type='derived-from')
+    sampl=event.add_object(sampl)
+    samples.update({hash:sampl})
     return sampl
 
-def getSampleDetail(sha256_hash):
+
+def _getSampleJson(sha256_hash):
     details_query={'query':'get_info', 'hash':'{}'.format(sha256_hash)}
     details_request=requests.post(bazaar_url, data=details_query)
 
@@ -49,14 +124,11 @@ def getSampleDetail(sha256_hash):
 
     return None
 
-# a dict of samples to add to the Event, keyed with <k>sha256 containing <v>MISP-Object Files
+# a dict of samples in the Event, keyed with <k>sha256 containing <v>MISP-Object Files
 samples={}
-# a dict of samples already in the Event, keyed with <k>sha256 containing <v>MISP-Object Files
-existing_samples={}
 
-# a cache of all the attributes to be added to the event
-existing_attributes = {}
-attributes = {}
+# a cache of all the attributes in the event
+attributes={}
 
 #misp setup
 pm = PyMISP(misp_url, misp_key, ssl=misp_check_cert)
@@ -66,14 +138,15 @@ search=pm.search(controller='events', eventinfo=misp_event_name)
 if ( len(search) == 1):
     misp_event.load(search[0])
 
-    #load 'existing_samples' dictionary from misp_event
+    #load 'samples' dictionary from misp_event
     for obj in misp_event.get('Object'):
         if (obj.name == "file"):
-            existing_samples.update({obj.get('sha256') : obj})
+            existing_hash = obj.get_attributes_by_relation("sha256")[0]['value']
+            samples.update({existing_hash : obj})
 
     #reload 'attributes' dictionary from misp_event
     for attr in misp_event.get('Attribute'):
-        existing_attributes.update({attr.value : attr})
+        attributes.update({attr.value : attr})
 else:
     misp_event.info=misp_event_name
     pm.add_event(misp_event)
@@ -82,109 +155,10 @@ else:
 r = requests.post(bazaar_url, data=bazaar_query)
 bazaardata = json.loads(r.text)
 
-
 # add File objects/samples that don't currently exist
 for sample in bazaardata.get('data'):
     hash_id=sample["sha256_hash"]
-    if (hash_id not in existing_samples):
-        sample_object=genFileObj(sample)
-        samples.update({hash_id : sample_object})
+    if (hash_id not in samples):
+        addSampleByHash(hash_id, misp_event)
 
-# iterate through the list of samples
-#   - a copy of the 'samples' so that we can updating the
-#     dictionary while iterating through it; this allows for
-#     one level of sample crawling. i.e. all samples refered
-#     to by sampled tagged COVID-19 will be included in the Event
-for hash,sample in samples.copy().items():
-
-    # get a detailed report of the sample
-    details = getSampleDetail(hash)
-    if (details is not None):
-        # if it exists, add the comment
-        if ( 'comment' in details ):
-
-            comment=details['comment']
-            if ( comment is not None ) and (len(comment) > 0):
-                sample.add_attribute("text", value=comment, to_ids=False, disable_correlation=True)
-                for commattr in pm.freetext(misp_event, comment):
-                   newattr = MISPAttribute()
-                   if (commattr['value'] not in existing_attributes):
-                        if (commattr['value'] not in attributes):
-                            newattr.category = commattr['category']
-                            newattr.type = commattr['type']
-                            newattr.value = commattr['value']
-                            newattr.to_ids = commattr['to_ids']
-
-                            attributes.update({newattr.value : newattr})
-                        else:
-                            newattr=attributes[commattr['value']]
-                   else:
-                       newattr=existing_attributes[commattr['value']]
-
-                   sample.add_reference(referenced_uuid=newattr.uuid, relationship_type='related-to')
-
-        # find and add x-references
-        if ( 'file_information' in details):
-            info=details['file_information']
-
-            if (info is not None):
-                for context_set in info:
-                    context=context_set['context']
-                    value=context_set['value']
-
-                    print("context: {}, value: {}".format(context, value))
-
-                    if ( context in API_REF_CONTEXTS ):
-                        ref_uuid=""
-
-                        # if referenced sample is not already represented, create it and add to samples<dict>
-                        if ( value not in samples ):
-                            refSamp=getSampleDetail(value)
-                            if (refSamp is not None):
-                                refSampFile = genFileObj(refSamp)
-                                samples.update({value : refSampFile})
-
-                        if ( value in samples ):
-                            ref_uuid=samples[value].uuid
-
-                            if (context == "dropped_by_sha256"):
-                                sample.add_reference(referenced_uuid=ref_uuid, relationship_type='dropped-by')
-                            else:
-                                sample.add_reference(referenced_uuid=ref_uuid, relationship_type='drops')
-                    elif ( context.casefold() in API_LINK_CONTEXTS ):
-                        url_ref=value.replace('\\','')
-                        attribute = MISPAttribute()
-                        if ( url_ref not in existing_attributes):
-                            if ( url_ref not in attributes):
-                                attribute.category = "External analysis"
-                                attribute.type = "url"
-                                attribute.value = url_ref
-                                attribute.to_ids = False
-                                attribute.disable_correlation = True
-                                attributes.update({attribute.value : attribute})
-                            else:
-                                attribute=attributes[url_ref]
-                        else:
-                            attribute=existing_attributes[url_ref]
-
-                        sample.add_reference(referenced_uuid=attribute.uuid, relationship_type='related-to')
-                    else:
-                        print("Lost context: {}".format(context))
-
-        attribute = MISPAttribute()
-        attribute.category = "External analysis"
-        attribute.type = "url"
-        attribute.value = "https://bazaar.abuse.ch/sample/{}/".format(hash)
-        attribute.to_ids = False
-        attribute.disable_correlation = True
-        attributes.update({attribute.value : attribute})
-        sample.add_reference(referenced_uuid=attribute.uuid, relationship_type='derived-from')
-
-for value,attribute in attributes.items():
-    pm.add_attribute(misp_event, attribute)
-
-for hash,sample in samples.items():
-    misp_event.add_object(sample)
-
-
-event=pm.update_event(misp_event)
+pm.update_event(misp_event)
